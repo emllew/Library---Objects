c4fc7efde57a627979d3d46b60f7358da98683dc
Emily <emilyllewelyn@gmail.com>
2021-06-16 23:57:07 +0100

    log a book into an object

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 let myLibrary = [];
+let button = document.querySelector('button');
+let form = document.querySelector('form');
+
+button.addEventListener('click', () => {
+  form.setAttribute('class', 'showForm');
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let title = form.elements.title.value;
+  let author = form.elements.author.value;
+  let read = form.elements.read.value;
+  let pages = form.elements.pages.value;
+  console.log(title + author + read + pages);
+  addBookToLibrary(title, author, read, pages);
+});
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -9,15 +25,11 @@ function Book(title, author, pages, read) {
     return title + ' by ' + author + ', ' + pages + ' pages, ' + read;
   };
 }
-
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(title, author, read, pages) {
+  let newBook = new Book(title, author, read, pages);
+  console.log(newBook);
 }
 
-function Books
-
-const book1 = new Books('The Hobbit', 'Tolkein', '265', 'has been read');
+const book1 = new Book('The Hobbit', 'Tolkein', '265', 'has been read');
 console.log(book1.info());
 console.log(book1);
-
-
